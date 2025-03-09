a0a93f8153b368239294cecda6eb305674e476c7
stranger86952 <stranger.86952@gmail.com>
2025-03-09 12:09:12 +0900

    Prototype completed

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # sv-gen
+
+## Note
+  - **URL、'#'になる部分はデコードしちゃいけない**
+    - '#'以降の情報はアンカーとみなされてサーバーに送信されないし
+  - ズームが大きくないと道の粒度が下がってしまう問題
+    - 小さいので取得して合成するしかない
+
+## API
+- [このサイト](https://mapstyle.withgoogle.com/)でスタイルを編集できる
+  - 例えばroads-lower.jsonはURLだと以下のようになる
+    https://maps.googleapis.com/maps/api/staticmap?key=YOUR_API_KEY&center=47.65,-122.35&zoom=12&format=png&maptype=roadmap&style=feature:administrative%7Cvisibility:off&style=feature:landscape%7Cvisibility:off&style=feature:poi%7Cvisibility:off&style=feature:road%7Ccolor:0x99e9f2%7Cvisibility:simplified%7Cweight:7&style=feature:road%7Celement:labels%7Cvisibility:off&style=feature:transit%7Cvisibility:off&style=feature:water%7Cvisibility:off&size=480x360
+- 画像はタイル座標で提供されてる(ZLは1~20)
+  - ZL=nならx,yはそれぞれ0から2^n-1まで(原点は北西)
+  
---
+++ script.js
@@ -0,0 +1,158 @@
+/*
+# Hong Kong, 22.3193°N, 114.1694°E
+- label
+  - https://maps.googleapis.com/maps/vt?pb=%211m5%211m4%211i12%212i3346%213i1787%214i256%212m2%211e0%212sm%213m17%212sen%213sUS%215e18%2112m4%211e68%212m2%211sset%212sRoadmap%2112m3%211e37%212m1%211ssmartmaps%2112m4%211e26%212m2%211sstyles%212ss.t%3A17%7Cs.e%3Ag.s%7Cp.w%3A2%7Cp.c%3A%23000000%2Cs.t%3A18%7Cs.e%3Ag.s%7Cp.w%3A3%2Cs.e%3Ag%7Cp.v%3Aoff%2Cs.t%3A1%7Cs.e%3Ag.s%7Cp.v%3Aon%2Cs.e%3Al%7Cp.v%3Aon%214i0%215m2%211e0%215f2
+  - https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i12!2i3346!3i1787!4i256!2m2!1e0!2sm!3m17!2sen!3sUS!5e18!12m4!1e68!2m2!1sset!2sRoadmap!12m3!1e37!2m1!1ssmartmaps!12m4!1e26!2m2!1sstyles!2ss.t:17|s.e:g.s|p.w:2|p.c:%23000000,s.t:18|s.e:g.s|p.w:3,s.e:g|p.v:off,s.t:1|s.e:g.s|p.v:on,s.e:l|p.v:on!4i0!5m2!1e0!5f2
+- base
+  - https://maps.googleapis.com/maps/vt?pb=%211m5%211m4%211i12%212i3346%213i1787%214i256%212m2%211e0%212sm%213m17%212sen%213sUS%215e18%2112m4%211e68%212m2%211sset%212sRoadmap%2112m3%211e37%212m1%211ssmartmaps%2112m4%211e26%212m2%211sstyles%212ss.t%3A17%7Cs.e%3Ag.s%7Cp.w%3A2%7Cp.c%3A%23000000%2Cs.t%3A18%7Cs.e%3Ag.s%7Cp.w%3A3%2Cs.e%3Al%7Cp.v%3Aoff%2Cs.t%3A1%7Cs.e%3Ag.s%7Cp.v%3Aoff%214i0%215m2%211e0%215f2
+  - https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i12!2i3346!3i1787!4i256!2m2!1e0!2sm!3m17!2sen!3sUS!5e18!12m4!1e68!2m2!1sset!2sRoadmap!12m3!1e37!2m1!1ssmartmaps!12m4!1e26!2m2!1sstyles!2ss.t:17|s.e:g.s|p.w:2|p.c:%23000000,s.t:18|s.e:g.s|p.w:3,s.e:l|p.v:off,s.t:1|s.e:g.s|p.v:off!4i0!5m2!1e0!5f2
+- street
+  - https://maps.googleapis.com/maps/vt?pb=%211m5%211m4%211i12%212i3346%213i1787%214i256%212m8%211e2%212ssvv%214m2%211scc%212s*211m3*211e2*212b1*213e2*212b1*214b1%214m2%211ssvl%212s*212b1%213m17%212sen%213sUS%215e18%2112m4%211e68%212m2%211sset%212sRoadmap%2112m3%211e37%212m1%211ssmartmaps%2112m4%211e26%212m2%211sstyles%212ss.e%3Ag.f%7Cp.c%3A%231098ad%7Cp.w%3A1%2Cs.e%3Ag.s%7Cp.c%3A%2399e9f2%7Cp.w%3A3%215m1%215f2
+  - https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i12!2i3346!3i1787!4i256!2m8!1e2!2ssvv!4m2!1scc!2s*211m3*211e2*212b1*213e2*212b1*214b1!4m2!1ssvl!2s*212b1!3m17!2sen!3sUS!5e18!12m4!1e68!2m2!1sset!2sRoadmap!12m3!1e37!2m1!1ssmartmaps!12m4!1e26!2m2!1sstyles!2ss.e:g.f|p.c:%231098ad|p.w:1,s.e:g.s|p.c:%2399e9f2|p.w:3!5m1!5f2
+- roadUpper
+  - https://maps.googleapis.com/maps/vt?pb=%211m5%211m4%211i11%212i326%213i716%214i256%212m3%211e0%212sm%213i722480938%213m18%212sja%213sUS%215e18%2112m5%211e68%212m2%211sset%212sRoadmap%214e2%2112m3%211e37%212m1%211ssmartmaps%2112m4%211e26%212m2%211sstyles%212zcy50OjF8cC52Om9mZixzLnQ6NXxwLnY6b2ZmLHMudDoyfHAudjpvZmYscy50OjN8cC5jOiMxMDk4YWR8cC52OnNpbXBsaWZpZWR8cC53OjAuNSxzLnQ6M3xzLmU6bHxwLnY6b2ZmLHMudDo0fHAudjpvZmYscy50OjZ8cC52Om9mZg%214e0%215m2%211e3%215f2
+  - https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i12!2i3346!3i1787!4i256!2m3!1e0!2sm!3i722480938!3m18!2sja!3sUS!5e18!12m5!1e68!2m2!1sset!2sRoadmap!4e2!12m3!1e37!2m1!1ssmartmaps!12m4!1e26!2m2!1sstyles!2zcy50OjF8cC52Om9mZixzLnQ6NXxwLnY6b2ZmLHMudDoyfHAudjpvZmYscy50OjN8cC5jOiMxMDk4YWR8cC52OnNpbXBsaWZpZWR8cC53OjAuNSxzLnQ6M3xzLmU6bHxwLnY6b2ZmLHMudDo0fHAudjpvZmYscy50OjZ8cC52Om9mZg!4e0!5m2!1e3!5f2
+- roadLower
+  - https://maps.googleapis.com/maps/vt?pb=%211m5%211m4%211i12%212i3346%213i1787%214i256%212m3%211e0%212sm%213i722480986%213m18%212sja%213sUS%215e18%2112m5%211e68%212m2%211sset%212sRoadmap%214e2%2112m3%211e37%212m1%211ssmartmaps%2112m4%211e26%212m2%211sstyles%212zcy50OjF8cC52Om9mZixzLnQ6NXxwLnY6b2ZmLHMudDoyfHAudjpvZmYscy50OjN8cC5jOiM5OWU5ZjJ8cC52OnNpbXBsaWZpZWR8cC53Ojcscy50OjN8cy5lOmx8cC52Om9mZixzLnQ6NHxwLnY6b2ZmLHMudDo2fHAudjpvZmY%214e0%215m2%211e3%215f2
+  - https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i12!2i3346!3i1787!4i256!2m3!1e0!2sm!3i722480986!3m18!2sja!3sUS!5e18!12m5!1e68!2m2!1sset!2sRoadmap!4e2!12m3!1e37!2m1!1ssmartmaps!12m4!1e26!2m2!1sstyles!2zcy50OjF8cC52Om9mZixzLnQ6NXxwLnY6b2ZmLHMudDoyfHAudjpvZmYscy50OjN8cC5jOiM5OWU5ZjJ8cC52OnNpbXBsaWZpZWR8cC53Ojcscy50OjN8cy5lOmx8cC52Om9mZixzLnQ6NHxwLnY6b2ZmLHMudDo2fHAudjpvZmY!4e0!5m2!1e3!5f2
+*/
+
+function latLngToTile(lat, lng, zoom) {
+  const n = Math.pow(2, zoom);
+  const x = Math.floor((lng + 180) / 360 * n);
+  const latRad = lat * (Math.PI / 180);
+  const y = Math.floor((1 - Math.log(Math.tan(latRad) + 1 / Math.cos(latRad)) / Math.PI) / 2 * n);
+  return { x, y };
+}
+
+function overLayImages() {
+  const fakeCanvas = document.getElementById('fakeCanvas');
+  const ctx = fakeCanvas.getContext('2d');
+
+  const baseLayer = document.getElementById('baseCanvas');
+  const lowerLayer = document.getElementById('roadLowerCanvas');
+  const upperLayer = document.getElementById('roadUpperCanvas');
+  const labelLayer = document.getElementById('labelCanvas');
+
+  fakeCanvas.width = baseLayer.width;
+  fakeCanvas.height = baseLayer.height;
+
+  ctx.drawImage(baseLayer, 0, 0);
+  ctx.drawImage(lowerLayer, 0, 0);
+  ctx.drawImage(upperLayer, 0, 0);
+  ctx.drawImage(labelLayer, 0, 0);
+}
+
+function generate() {
+  // label & base Lowerground color seems like fixed
+  const labelLowergroundColor = '000000';
+  const baseLowergroundColor = '000000';
+  // default line & Lower color is 1098ad & 99e9f2
+  const streetLineColor = '1098ad';
+  const streetLowerColor = '99e9f2';
+  // user input
+  const zoom = parseInt(document.getElementById("zoom").value);
+  const areaX = parseInt(document.getElementById("areaX").value);
+  const areaY = parseInt(document.getElementById("areaY").value);
+  const lat = parseFloat(document.getElementById("lat").value);
+  const lng = parseFloat(document.getElementById("lng").value);
+  // canvas
+  const labelCanvas = document.getElementById('labelCanvas');
+  const labelCtx = labelCanvas.getContext('2d');
+  labelCanvas.width = 512 * areaX;
+  labelCanvas.height = 512 * areaY;
+  const baseCanvas = document.getElementById('baseCanvas');
+  const baseCtx = baseCanvas.getContext('2d');
+  baseCanvas.width = 512 * areaX;
+  baseCanvas.height = 512 * areaY;
+  const streetCanvas = document.getElementById('streetCanvas');
+  const streetCtx = streetCanvas.getContext('2d');
+  streetCanvas.width = 512 * areaX;
+  streetCanvas.height = 512 * areaY;
+  const roadUpperCanvas = document.getElementById('roadUpperCanvas');
+  const roadUpperCtx = roadUpperCanvas.getContext('2d');
+  roadUpperCanvas.width = 512 * areaX;
+  roadUpperCanvas.height = 512 * areaY;
+  const roadLowerCanvas = document.getElementById('roadLowerCanvas');
+  const roadLowerCtx = roadLowerCanvas.getContext('2d');
+  roadLowerCanvas.width = 512 * areaX;
+  roadLowerCanvas.height = 512 * areaY;
+
+  // tiles
+  let { x, y } = latLngToTile(lat, lng, zoom);
+  let { xSub, ySub } = latLngToTile(lat, lng, zoom + 1);
+  x -= xSub % 2 == 0 ? (areaX - areaX % 2) / 2 : (areaX + areaX % 2) / 2 - 1;
+  y -= ySub % 2 == 0 ? (areaY - areaY % 2) / 2 : (areaY + areaY % 2) / 2 - 1;
+  let loaded = 0;
+  const maxLoad = areaX * areaY * 5
+
+  // load
+  for (let i = 0; i < areaX; i++) {
+    for (let j = 0; j < areaY; j++) {
+      // URL
+      const labelURL = `https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i${zoom}!2i${x + i}!3i${y + j}!4i256!2m2!1e0!2sm!3m17!2sen!3sUS!5e18!12m4!1e68!2m2!1sset!2sRoadmap!12m3!1e37!2m1!1ssmartmaps!12m4!1e26!2m2!1sstyles!2ss.t:17|s.e:g.s|p.w:2|p.c:%23${labelLowergroundColor},s.t:18|s.e:g.s|p.w:3,s.e:g|p.v:off,s.t:1|s.e:g.s|p.v:on,s.e:l|p.v:on!4i0!5m2!1e0!5f2`;
+      const baseURL = `https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i${zoom}!2i${x + i}!3i${y + j}!4i256!2m2!1e0!2sm!3m17!2sen!3sUS!5e18!12m4!1e68!2m2!1sset!2sRoadmap!12m3!1e37!2m1!1ssmartmaps!12m4!1e26!2m2!1sstyles!2ss.t:17|s.e:g.s|p.w:2|p.c:%23${baseLowergroundColor},s.t:18|s.e:g.s|p.w:3,s.e:l|p.v:off,s.t:1|s.e:g.s|p.v:off!4i0!5m2!1e0!5f2`;
+      const streetURL = `https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i${zoom}!2i${x + i}!3i${y + j}!4i256!2m8!1e2!2ssvv!4m2!1scc!2s*211m3*211e2*212b1*213e2*212b1*214b1!4m2!1ssvl!2s*212b1!3m17!2sen!3sUS!5e18!12m4!1e68!2m2!1sset!2sRoadmap!12m3!1e37!2m1!1ssmartmaps!12m4!1e26!2m2!1sstyles!2ss.e:g.f|p.c:%23${streetLineColor}|p.w:1,s.e:g.s|p.c:%23${streetLowerColor}|p.w:3!5m1!5f2`;
+      const roadUpperURL = `https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i${zoom}!2i${x + i}!3i${y + j}!4i256!2m3!1e0!2sm!3i722480938!3m18!2sja!3sUS!5e18!12m5!1e68!2m2!1sset!2sRoadmap!4e2!12m3!1e37!2m1!1ssmartmaps!12m4!1e26!2m2!1sstyles!2zcy50OjF8cC52Om9mZixzLnQ6NXxwLnY6b2ZmLHMudDoyfHAudjpvZmYscy50OjN8cC5jOiMxMDk4YWR8cC52OnNpbXBsaWZpZWR8cC53OjAuNSxzLnQ6M3xzLmU6bHxwLnY6b2ZmLHMudDo0fHAudjpvZmYscy50OjZ8cC52Om9mZg!4e0!5m2!1e3!5f2`;
+      const roadLowerURL = `https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i${zoom}!2i${x + i}!3i${y + j}!4i256!2m3!1e0!2sm!3i722480986!3m18!2sja!3sUS!5e18!12m5!1e68!2m2!1sset!2sRoadmap!4e2!12m3!1e37!2m1!1ssmartmaps!12m4!1e26!2m2!1sstyles!2zcy50OjF8cC52Om9mZixzLnQ6NXxwLnY6b2ZmLHMudDoyfHAudjpvZmYscy50OjN8cC5jOiM5OWU5ZjJ8cC52OnNpbXBsaWZpZWR8cC53Ojcscy50OjN8cy5lOmx8cC52Om9mZixzLnQ6NHxwLnY6b2ZmLHMudDo2fHAudjpvZmY!4e0!5m2!1e3!5f2`;
+
+      // dummy
+      const labelImg = new Image();
+      labelImg.src = labelURL;
+      labelImg.crossOrigin = "anonymous";
+      const baseImg = new Image();
+      baseImg.src = baseURL;
+      baseImg.crossOrigin = "anonymous";
+      const streetImg = new Image();
+      streetImg.src = streetURL;
+      streetImg.crossOrigin = "anonymous";
+      const roadUpperImg = new Image();
+      roadUpperImg.src = roadUpperURL;
+      roadUpperImg.crossOrigin = "anonymous";
+      const roadLowerImg = new Image();
+      roadLowerImg.src = roadLowerURL;
+      roadLowerImg.crossOrigin = "anonymous";
+
+      // draw
+      labelImg.onload = function () {
+        labelCtx.drawImage(labelImg, i * 512, j * 512);
+        loaded++;
+        if (maxLoad == loaded) {
+          overLayImages();
+        }
+      };
+      baseImg.onload = function () {
+        baseCtx.drawImage(baseImg, i * 512, j * 512);
+        loaded++;
+        if (maxLoad == loaded) {
+          overLayImages();
+        }
+      };
+      streetImg.onload = function () {
+        streetCtx.drawImage(streetImg, i * 512, j * 512);
+        loaded++;
+        if (maxLoad == loaded) {
+          overLayImages();
+        }
+      };
+      roadUpperImg.onload = function () {
+        roadUpperCtx.drawImage(roadUpperImg, i * 512, j * 512);
+        loaded++;
+        if (maxLoad == loaded) {
+          overLayImages();
+        }
+      };
+      roadLowerImg.onload = function () {
+        roadLowerCtx.drawImage(roadLowerImg, i * 512, j * 512);
+        loaded++;
+        if (maxLoad == loaded) {
+          overLayImages();
+        }
+      };
+    }
+  }
+}
+
+window.onload = function () {
+  generate();
+};
